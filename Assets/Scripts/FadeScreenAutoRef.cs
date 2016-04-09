263cb024eb0d4dcf5bf59adc3eef1d0a1a682b80
Tiras69 <mathm69@gmail.com>
2016-04-09 15:29:45 +0200

    Game Manager and some other shit

---
+++ Assets/Scripts/FadeScreenAutoRef.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeScreenAutoRef : MonoBehaviour {
+
+    private Image fadeScreen;
+    private Color _transparentColor;
+    private Color _opaqueColor;
+    public const float _fadeTime = 3.0f;
+    private float _fadeTimer;
+
+    // Use this for initialization
+    void Start () {
+        GameManager.Instance.fadeScreen = this;
+        fadeScreen = GetComponent<Image>();
+        FadeIn();
+	}
+
+    public void FadeIn()
+    {
+        _opaqueColor = new Color(fadeScreen.color.r,
+                                    fadeScreen.color.g,
+                                    fadeScreen.color.b,
+                                    fadeScreen.color.a);
+
+        _transparentColor = new Color(fadeScreen.color.r,
+                                        fadeScreen.color.g,
+                                        fadeScreen.color.b,
+                                        0.0f);
+
+        StopCoroutine(FadeInDelayed());
+        _fadeTimer = 0.0f;
+        StartCoroutine(FadeInDelayed());
+        }
+        
+
+    private IEnumerator FadeInDelayed()
+    {
+        while (_fadeTimer < _fadeTime)
+        {
+            _fadeTimer = _fadeTimer + Time.deltaTime;
+            fadeScreen.color = Color.Lerp(_opaqueColor, _transparentColor, _fadeTimer / _fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    public void FadeOut()
+    {
+
+    }
+
+    private IEnumerator FadeOutDelayed()
+    {
+        while (_fadeTimer < _fadeTime)
+        {
+            _fadeTimer = _fadeTimer + Time.deltaTime;
+            fadeScreen.color = Color.Lerp( _transparentColor, _opaqueColor, _fadeTimer / _fadeTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : Singleton<GameManager> {
+
+    public FadeScreenAutoRef fadeScreen;
+    public string currentScene;
+
+   
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLevel(string levelName)
+    {
+
+    }
+
+    
+
+}
