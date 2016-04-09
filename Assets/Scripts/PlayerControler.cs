b326db261f2bdebb485b70996f350b23b85a1b49
Madahin <nico99999@hotmail.fr>
2016-04-09 14:01:23 +0200

    Fix hierarchy

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+    [Range(0.0f, 10.0f)]
+    public float MoveSpeedFactor = 0.5f;
+
+    [Range(0.0f, 10.0f)]
+    public float MouseSpeedFactor = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Translate(Input.GetAxis("Horizontal") * MoveSpeedFactor,
+                                       0f,
+                                       Input.GetAxis("Vertical") * MoveSpeedFactor);
+
+        gameObject.transform.Rotate(-Input.GetAxis("Mouse Y"),
+                                    Input.GetAxis("Mouse X"),
+                                    0f);
+
+	}
+}
