8dea006778150e63b2a1e775c5ee99421ed845bf
pguimaraes1234567890 <dragon_killer_68@hotmail.fr>
2016-04-09 21:25:29 +0200

    barbieLife

---
+++ Assets/Scripts/BarbieLife.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarbieLife : MonoBehaviour {
+    public int life;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void decreaseLife(int degats)
+    {
+        // check si barbie die
+        life = life - degats;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+       if ( col.gameObject.tag == "projectile")
+        {
+            // dégat a changer
+            this.decreaseLife(1);
+        }
+    }
+}
