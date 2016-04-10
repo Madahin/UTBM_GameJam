beba53184f78cb0a3bd109f75cf0a2890f39c98d
Tiras69 <mathm69@gmail.com>
2016-04-10 03:44:04 +0200

    ZIZI COMIT DU WHOLOLO

---
+++ Assets/Scripts/CadreVieAutoRef.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CadreVieAutoRef : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameManager.Instance.CadreVie = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
