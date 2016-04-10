de9d705fc775cfcd224680622287c1297bae3446
Tiras69 <mathm69@gmail.com>
2016-04-10 06:44:18 +0200

    PATATOID de mes couilles en ski

---
+++ Assets/Scripts/FeeScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FeeScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<BarbieLife>().life = 5;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0.0f, 10.0f * Time.deltaTime, 0.0f);
+	}
+
+    public void StartGame()
+    {
+        GameManager.Instance.LoadLevel("Level1");
+    }
+}
