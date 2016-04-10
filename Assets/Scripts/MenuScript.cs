aceff901146105bdca1a7b2f2c040730b864506d
Tiras69 <mathm69@gmail.com>
2016-04-10 09:53:42 +0200

    Death lol

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,8 +5,9 @@ public class MenuScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
